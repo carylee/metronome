560bc587566b54532132b8a2c9fc562b9a0d3477
Cary Lee <carylee@gmail.com>
2010-08-21 22:09:36 -0500

    Total visual overhaul.

--- js/metronome.js
+++ js/metronome.js
@@ -1,5 +1,6 @@
 var metronome = (function() {
-  var tempo = 280;
+  var display;
+  var tempo = 60;
   var playback;
   //var tick = document.getElementById("tick");
   //var tock = document.getElementById("tock");
@@ -10,15 +11,19 @@ var metronome = (function() {
   var ticks = [
     new Audio("audio/tick.mp3"),
     new Audio("audio/tock.mp3"),
-    //new Audio("audio/tick.mp3"),
-    //new Audio("audio/tock.mp3"),
+    new Audio("audio/tick.mp3"),
+    new Audio("audio/tock.mp3"),
+    new Audio("audio/tick.mp3"),
+    new Audio("audio/tock.mp3"),
   ]
   var tickIndex = 0;
   var beat = function() {
     ticks[tickIndex].play();
     tickIndex = (tickIndex + 1) % 2;
+    display.toggle();
   }
   var start = function() {
+    stop();
     var ms = 60/tempo * 1000;
     playback = setInterval( beat, ms );
   }
@@ -27,8 +32,30 @@ var metronome = (function() {
   }
   var setTempo = function( bpm ) {
     stop();
-    tempo = bpm;
+    if( !isNaN(bpm) && bpm ) {
+      tempo = bpm;
+    }
   }
+  var canvas;
+  var ctx;
+  $(document).ready( function() {
+    canvas = $("#visual canvas")[0];
+    ctx = canvas.getContext('2d');
+  });
+  var display = (function(){
+    var state = 0;
+    var draw = function( state ) {
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      var width = canvas.width/2;
+      ctx.fillRect(state*width,0,state*width+width,canvas.height);
+    }
+    var toggle = function() {
+      draw( state );
+      state = (state + 1) % 2;
+    }
+    return { toggle : toggle };
+  }());
+
   return { 
     start : start,
     setTempo : setTempo,
@@ -39,5 +66,14 @@ $(document).ready( function() {
 $("#start").click( function() {
   metronome.setTempo( $("#tempo").val() );
   metronome.start();
+  });
+$("div.button").toggle( function(){
+    metronome.setTempo( $("#tempo").val() );
+    metronome.start();
+    $(this).children("span").html("Stop");
+  },
+  function() {
+    metronome.stop();
+    $(this).children("span").html("Start");
   });
 });
